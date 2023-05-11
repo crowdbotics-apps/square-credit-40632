f22a763803debc48ab17d1eb5537b283e44bab59
Crowdbotics <team@crowdbotics.com>
2023-05-11 20:21:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-credit-40632",
+  url: "https://square-credit-40632.botics.co",
+  api: "https://square-credit-40632.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
